c1cf29ad0c1018b290cfb0a340126005e6d7c2c0
Narendra Darla <naren.darla@gmail.com>
2021-03-04 16:40:05 -0800

    POC to capture Image from WebView

---
+++ mylib/src/main/java/com/narenrecw/mylib/MyClass.kt
@@ -0,0 +1,4 @@
+package com.narenrecw.mylib
+
+class MyClass {
+}
---
+++ mylibrary/src/main/java/com/narenrecw/mylibrary/Example.java
@@ -0,0 +1,12 @@
+package com.narenrecw.mylibrary;
+
+import android.util.Log;
+
+public class Example {
+
+    private static final String TAG = Example.class.getSimpleName();
+
+    public static void d(){
+        Log.v(TAG,"Example");
+    }
+}
